93819949c73345b661f94ddd3235132fcca05972
Cory <coryf65@gmail.com>
2021-01-18 21:07:08 -0600

    Player can now move
    > New Player Controller script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cory.SumoBall
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public float speed = 5f;
+
+        private Rigidbody playerRb = null;
+        private GameObject focalPoint = null;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            playerRb = GetComponent<Rigidbody>();
+            focalPoint = GameObject.Find("Focal Point");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // get players input
+            float forwardInput = Input.GetAxis("Vertical");
+
+            // move the player object
+            playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+        }
+    }
+}
