166dcd641918d7885081a616eac7a8245ed616a9
Cory <coryf65@gmail.com>
2021-01-19 21:16:29 -0600

    Creating a powerup prefab

---
+++ Assets/Scripts/Powerup.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cory.SumoBall
+{
+    public class Powerup : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
